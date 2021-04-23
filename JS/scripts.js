b284e206a6d7e1c104b0a35dad95069d9bbdb7dc
DrPepper17 <ry_mockmann@yahoo.com>
2021-04-23 01:17:22 -0500

    423 115 Finished Cafe and Studies pages. Finished site Footer. Also commented out Books, Authors Contact, Links and Garden tab for tempoary. For now they may be a bit out of scope for the project.

---
+++ JS/scripts.js
@@ -0,0 +1,29 @@
+$(function() {
+    $("#bookCrimson").click(function(){
+        $("#roomModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#bookIndigo").click(function(){
+        $("#roomModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#bookNeon").click(function(){
+        $("#roomModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#bookOak").click(function(){
+        $("#roomModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#bookMaple").click(function(){
+        $("#roomModal").modal("show");
+    });
+});
